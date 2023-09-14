b3fc7f0c44b8b77a6e4bac002409053139d463c9
Sara Hassan <92973686+madcode99@users.noreply.github.com>
2023-09-14 01:44:20 -0700

    Add files via upload

---
+++ Day 1 - 10/Day 7_Expand Cards/index.js
@@ -0,0 +1,16 @@
+const card1 = document.getElementById('card-el-1')
+card1.addEventListener('click' , function(){
+    card1.classList.toggle('expand')
+})
+const card2 = document.getElementById('card-el-2')
+card2.addEventListener('click' , function(){
+    card2.classList.toggle('expand')
+})
+const card3 = document.getElementById('card-el-3')
+card3.addEventListener('click' , function(){
+    card3.classList.toggle('expand')
+})
+const card4 = document.getElementById('card-el-4')
+card4.addEventListener('click' , function(){
+    card4.classList.toggle('expand')
+})
